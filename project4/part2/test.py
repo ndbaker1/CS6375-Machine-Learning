a31dfb8716c76719b08abe8eee1bd2bcd6c56138
Nicholas Baker <ndbaker1@outlook.com>
2023-04-29 14:13:45 -0500

    update and parametrize tests

--- project4/part2/test.py
+++ project4/part2/test.py
@@ -17,42 +17,70 @@ print(f"{len(dataset_names)} datasets: {dataset_names}")
 
 # run each algorithm 5 times and compute the average and standard deviation
 
-for dataset_name in dataset_names:
+k_options = [2,5,10,20]
+r_options = [0.05, 0.1, 0.2]
+num_runs = 5
+
+def load_dataset(dataset_name):
     print(f"loading dataset [{dataset_name}]")
     test, train, valid = [
         Util.load_dataset(f"{DATASET_DIR}/{dataset_name}.{t}.data")
         for t in ("test", "ts", "valid")
     ]
+    return test, train, valid
 
+
+for dataset_name in dataset_names:
+    test, train, valid = load_dataset(dataset_name)
     print("running CLT... ", end="")
     clt = CLT()
     clt.learn(train)
-    ll = clt.computeLL(test) / test.shape[0]
-    print("ll:", ll)
+    prev_ll = clt.computeLL(test) / test.shape[0]
+    print("ll:", prev_ll)
 
+
+for dataset_name in dataset_names:
+    test, train, valid = load_dataset(dataset_name)
     print("running MIXTURE_CLT... ", end="")
-    clt, ll, k_opt = MIXTURE_CLT(), -np.inf, 0
-    for k in (2, 5, 10, 20):
+    prev_ll, k_opt = -np.inf, 0
+    for k in k_options:
         clt_ = MIXTURE_CLT()
         clt_.learn(train, n_components=k)
         # use validation dataset to compare K values
         ll_ = clt_.computeLL(valid) / valid.shape[0]
         # keep best performing model
-        if ll_ > ll:
-            clt, ll, k_opt = clt_, ll_, k
-    ll = clt.computeLL(test) / test.shape[0]
-    print("ll:", ll, "k:", k_opt)
+        if ll_ > prev_ll:
+            prev_ll, k_opt = ll_, k
+    
+    runs = []
+    for _ in range(num_runs):
+        clt = MIXTURE_CLT()
+        clt.learn(test, n_components=k_opt)
+        runs.append(clt.computeLL(test) / test.shape[0])
+    mean_ll = np.mean(runs)
+    std_ll = np.std(runs)
+    print("ll:", mean_ll, "std:", std_ll, "k:", k_opt)
 
+
+for dataset_name in dataset_names:
+    test, train, valid = load_dataset(dataset_name)
     print("running RANDOM_FOREST_CLT... ", end="")
-    clt, ll, k_opt, r_opt = RANDOM_FOREST_CLT(), -np.inf, 0, 0
-    for k in (2, 5, 10, 20):
-        for r in (0.05, 0.1, 0.2):
+    prev_ll, k_opt, r_opt = -np.inf, 0, 0
+    for k in k_options:
+        for r in r_options:
             clt_ = RANDOM_FOREST_CLT()
             clt_.learn(train, k, r)
             # use validation dataset to compare K values
             ll_ = clt_.computeLL(valid) / valid.shape[0]
             # keep best performing model
-            if ll_ > ll:
-                clt, ll, k_opt, r_opt = clt_, ll_, k, r
-    ll = clt.computeLL(test) / test.shape[0]
-    print("ll:", ll, "k:", k_opt, "r:", r_opt)
+            if ll_ > prev_ll:
+                clt, prev_ll, k_opt, r_opt = clt_, ll_, k, r
+    
+    runs = []
+    for _ in range(num_runs):
+        clt = RANDOM_FOREST_CLT()
+        clt.learn(test, k=k_opt, r=r_opt)
+        runs.append(clt.computeLL(test) / test.shape[0])
+    mean_ll = np.mean(runs)
+    std_ll = np.std(runs)
+    print("ll:", mean_ll, "std:", std_ll, "k:", k_opt, "r:", r_opt)
